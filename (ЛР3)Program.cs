8779616ae3ff288f7761c283947df262cb2b62d5
Kamil-Tushakov <72381143+Kamil-Tushakov@users.noreply.github.com>
2020-10-05 13:51:12 +0500

    Add files via upload

---
+++ (ЛР3)Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лабораторная_работа__3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[20];
+            Random rand = new Random();
+            for (int y = 0; y < 20; y++)
+            {
+                array[y] = rand.Next(-100, 100);
+                {
+                    Console.WriteLine(array[y]);
+                  
+                    Console.ReadLine();
+                }     
+            }
+        }
+    }
+}
