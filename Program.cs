f36ff9af6ccc6c1bfe41ddd0c8aae3cd6921e7b8
Kamil-Tushakov <72381143+Kamil-Tushakov@users.noreply.github.com>
2020-10-05 13:48:48 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,17 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Лабораторная_работа__1
+namespace Лабораторная_работа__2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Лабараторная работа № 1 || laboratory work № 1");
+            int[] nums = new int[5];
+            nums[0] = 1;
+            nums[1] = 2;
+            nums[2] = 3;
+            nums[3] = 4;
+            nums[4] = 5;
+            Console.WriteLine(nums[3] + nums[0]); // попросим вывести решение арифметического примера
             Console.WriteLine("");
-            Console.WriteLine("Плавный процесс обучения в ТИУ || Smooth the learning process in TIU"); 
-            Console.ReadLine(); // без этой команды текст в консоль не выводится(
-
+            Console.WriteLine(nums[1]);
+            Console.WriteLine("+");
+            Console.WriteLine(nums[2]);
+            Console.WriteLine("=");
+            Console.WriteLine(nums[4]);
+            Console.ReadLine();
         }
     }
 }
