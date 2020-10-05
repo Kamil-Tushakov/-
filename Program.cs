f8c9f28eba2d4417c859edcbb39307e0d276eacb
Kamil-Tushakov <72381143+Kamil-Tushakov@users.noreply.github.com>
2020-10-05 13:48:16 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лабораторная_работа__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабараторная работа № 1 || laboratory work № 1");
+            Console.WriteLine("");
+            Console.WriteLine("Плавный процесс обучения в ТИУ || Smooth the learning process in TIU"); 
+            Console.ReadLine(); // без этой команды текст в консоль не выводится(
+
+        }
+    }
+}
